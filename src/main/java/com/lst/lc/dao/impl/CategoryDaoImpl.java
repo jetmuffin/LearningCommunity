b5e0e9d2fa06a0bccffdc2d6e950f853145110dc
sunzequn <sunzequn@live.cn>
2015-07-13 19:34:18 +0800

    修改分类实体和控制器

--- src/main/java/com/lst/lc/dao/impl/CategoryDaoImpl.java
+++ src/main/java/com/lst/lc/dao/impl/CategoryDaoImpl.java
@@ -2,6 +2,7 @@ package com.lst.lc.dao.impl;
 
 import java.util.List;
 
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lst.lc.dao.CategoryDao;
@@ -24,15 +25,23 @@ public class CategoryDaoImpl extends BaseDao implements CategoryDao {
 	public List<Category> getAllCategories() {
 		return getAll("Category");
 	}
-	
+
 	@Override
-	public void update(Category category){
-		saveOrUpdate(category);
+	public void update(int categoryId, String categoryName, String description, String enabled,
+			int directionId) {
+		String hql = "update Category as category set category.categoryName = ?, category.description = ?, category.enabled = ?, directionId = ? where categoryId = ?";
+		Query query = query(hql);
+		query.setString(0, categoryName).setString(1, description)
+				.setString(2, enabled).setInteger(3, directionId).setInteger(4, categoryId)
+				.executeUpdate();
+	}
+
+	@Override
+	public void delete(int categoryId) {
+		String hql = "delete Category as category where category.categoryId = ?";
+		Query query = query(hql);
+		query.setInteger(0, categoryId).executeUpdate();
 	}
 	
-	@Override
-	public void delete(Category category){
-		delete(category);
-	}
-	
+
 }
