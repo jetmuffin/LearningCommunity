1dfa1420a2e121fabfe5f45bd8133f950f342534
InnerAc <anjicun@live.com>
2015-07-14 10:40:31 +0800

    增加工具类-获取当前项目路径

---
+++ src/test/java/com/lst/lc/test/onlyfortest/GetPathTest.java
@@ -0,0 +1,11 @@
+package com.lst.lc.test.onlyfortest;
+
+import org.junit.Test;
+
+public class GetPathTest {
+
+	@Test
+	public void getPahtTest(){
+		System.out.println("path = "+System.getProperty("user.dir"));
+	}
+}
